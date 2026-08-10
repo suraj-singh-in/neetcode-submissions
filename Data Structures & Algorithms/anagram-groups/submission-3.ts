c88e2dc221eb2a34f187f02b1b8b0a381279902d
Suraj Singh <136962247+suraj-singh-in@users.noreply.github.com>
2026-08-10 21:50:55 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.ts
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+
+    groupAnagrams(strs: string[]): string[][] {
+        let hashMap = {};
+
+        for (let index = 0; index < strs.length; index += 1) {
+            let key = strs[index];
+            let value = {};
+            let sortedCharacters = key.split("").sort();
+
+            for (let innerIndex = 0; innerIndex < sortedCharacters.length; innerIndex += 1) {
+                let ele = sortedCharacters[innerIndex];
+                if (value[ele]) {
+                    value[ele] = value[ele] + 1;
+                } else {
+                    value[ele] = 1;
+                }
+            }
+
+            if (hashMap[key]) {
+            }
+
+            const valKey = Object.keys(value)
+                .sort()
+                .map((char) => `${char}${value[char]}`)
+                .join("");
+
+            if (!hashMap[valKey]) {
+                hashMap[valKey] = [];
+            }
+
+            hashMap[valKey].push(key);
+        }
+
+
+        return Object.values(hashMap);
+    }
+}
